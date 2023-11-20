87cd02f5eb7876c78e4417a83aa1d7617b3f43d1
Anubhav Vaish <anubhavvaish04@gmail.com>
2023-11-20 23:25:56 +0530

    first commit

---
+++ ASPCoreWebAPICRUD/Controllers/StudentAPIController.cs
@@ -0,0 +1,70 @@
+﻿using ASPCoreWebAPICRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPICRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly CodeFirstDBContext context;
+
+        public StudentAPIController(CodeFirstDBContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<string>>> GetStudent()
+        {
+            var data = await context.Students.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentById(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+            
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student student)
+        {
+            await context.Students.AddAsync(student);
+            await context.SaveChangesAsync();
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Student>> UpdateStudent(int id, Student std)
+        {
+            if (id != std.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(std).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            context.Students.Remove(student);
+            await context.SaveChangesAsync();
+            return Ok(student);
+        }
+    }
+}
